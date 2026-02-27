a17fd10151283e98ba66148fa42378028f40771c
stagas <gstagas@gmail.com>
2026-02-27 15:17:32 +0200

    commit build

---
+++ as/build/index-mobile.d.ts
@@ -0,0 +1,241 @@
+/** Exported memory */
+export declare const memory: WebAssembly.Memory;
+// Exported runtime interface
+export declare function __new(size: number, id: number): number;
+export declare function __pin(ptr: number): number;
+export declare function __unpin(ptr: number): void;
+export declare function __collect(): void;
+export declare const __rtti_base: number;
+/**
+ * as/assembly/index/createFloat32Buffer
+ * @param size `i32`
+ * @returns `usize`
+ */
+export declare function createFloat32Buffer(size: number): number;
+/**
+ * as/assembly/index/freeFloat32Buffer
+ * @param ptr `usize`
+ */
+export declare function freeFloat32Buffer(ptr: number): void;
+/**
+ * as/assembly/index/memoryUsage
+ * @returns `usize`
+ */
+export declare function memoryUsage(): number;
+/**
+ * as/assembly/index/memoryGrow
+ * @param delta `i32`
+ * @returns `i32`
+ */
+export declare function memoryGrow(delta: number): number;
+/**
+ * as/assembly/index/getFloat32BufferArenaInfo
+ * @returns `usize`
+ */
+export declare function getFloat32BufferArenaInfo(): number;
+/**
+ * as/assembly/index/runAudioVm
+ * @param opsPtr `usize`
+ * @param opsLength `i32`
+ * @param bufferLength `i32`
+ * @param sampleCount `i32`
+ * @param sampleRate `f32`
+ * @param nyquist `f32`
+ * @param piOverNyquist `f32`
+ * @param bpm `f32`
+ */
+export declare function runAudioVm(opsPtr: number, opsLength: number, bufferLength: number, sampleCount: number, sampleRate: number, nyquist: number, piOverNyquist: number, bpm: number): void;
+/**
+ * as/assembly/index/getAudioVmInfo
+ * @returns `usize`
+ */
+export declare function getAudioVmInfo(): number;
+/**
+ * as/assembly/index/runAudioVmAt
+ * @param vmId `i32`
+ * @param opsPtr `usize`
+ * @param opsLength `i32`
+ * @param bufferLength `i32`
+ * @param sampleCount `i32`
+ * @param sampleRate `f32`
+ * @param nyquist `f32`
+ * @param piOverNyquist `f32`
+ * @param bpm `f32`
+ */
+export declare function runAudioVmAt(vmId: number, opsPtr: number, opsLength: number, bufferLength: number, sampleCount: number, sampleRate: number, nyquist: number, piOverNyquist: number, bpm: number): void;
+/**
+ * as/assembly/index/getAudioVmInfoAt
+ * @param vmId `i32`
+ * @returns `usize`
+ */
+export declare function getAudioVmInfoAt(vmId: number): number;
+/**
+ * as/assembly/index/getAudioVmArenaInfoAt
+ * @param vmId `i32`
+ * @returns `usize`
+ */
+export declare function getAudioVmArenaInfoAt(vmId: number): number;
+/**
+ * as/assembly/index/releaseAudioVmOutputsAt
+ * @param vmId `i32`
+ */
+export declare function releaseAudioVmOutputsAt(vmId: number): void;
+/**
+ * as/assembly/index/copyAudioVmState
+ * @param fromVmId `i32`
+ * @param toVmId `i32`
+ */
+export declare function copyAudioVmState(fromVmId: number, toVmId: number): void;
+/**
+ * as/assembly/index/resetAudioVmAt
+ * @param vmId `i32`
+ */
+export declare function resetAudioVmAt(vmId: number): void;
+/**
+ * as/assembly/index/getAudioVmGlobalAt
+ * @param vmId `i32`
+ * @param index `i32`
+ * @returns `f32`
+ */
+export declare function getAudioVmGlobalAt(vmId: number, index: number): number;
+/**
+ * as/assembly/index/setAudioVmGlobalAt
+ * @param vmId `i32`
+ * @param index `i32`
+ * @param value `f32`
+ */
+export declare function setAudioVmGlobalAt(vmId: number, index: number, value: number): void;
+/**
+ * as/assembly/index/setAudioVmGlobalUndefined
+ * @param vmId `i32`
+ * @param index `i32`
+ */
+export declare function setAudioVmGlobalUndefined(vmId: number, index: number): void;
+/**
+ * as/assembly/index/setAudioVmGlobalsSize
+ * @param vmId `i32`
+ * @param size `i32`
+ */
+export declare function setAudioVmGlobalsSize(vmId: number, size: number): void;
+/**
+ * as/assembly/index/getAudioVmArrayElementAt
+ * @param vmId `i32`
+ * @param arrayGlobalIndex `i32`
+ * @param elementIndex `i32`
+ * @returns `f32`
+ */
+export declare function getAudioVmArrayElementAt(vmId: number, arrayGlobalIndex: number, elementIndex: number): number;
+/**
+ * as/assembly/index/getAudioVmArrayElementIsUndefined
+ * @param vmId `i32`
+ * @param arrayGlobalIndex `i32`
+ * @param elementIndex `i32`
+ * @returns `bool`
+ */
+export declare function getAudioVmArrayElementIsUndefined(vmId: number, arrayGlobalIndex: number, elementIndex: number): boolean;
+/**
+ * as/assembly/index/getAudioVmNestedArrayElementAt
+ * @param vmId `i32`
+ * @param outerArrayGlobalIndex `i32`
+ * @param outerIndex `i32`
+ * @param innerIndex `i32`
+ * @returns `f32`
+ */
+export declare function getAudioVmNestedArrayElementAt(vmId: number, outerArrayGlobalIndex: number, outerIndex: number, innerIndex: number): number;
+/**
+ * as/assembly/index/getAudioVmNestedArrayElementIsUndefined
+ * @param vmId `i32`
+ * @param outerArrayGlobalIndex `i32`
+ * @param outerIndex `i32`
+ * @param innerIndex `i32`
+ * @returns `bool`
+ */
+export declare function getAudioVmNestedArrayElementIsUndefined(vmId: number, outerArrayGlobalIndex: number, outerIndex: number, innerIndex: number): boolean;
+/**
+ * as/assembly/index/setAudioVmOversampleModes
+ * @param vmId `i32`
+ * @param upMode `i32`
+ * @param downMode `i32`
+ */
+export declare function setAudioVmOversampleModes(vmId: number, upMode: number, downMode: number): void;
+/**
+ * as/assembly/index/setAudioVmPreserveFunctionState
+ * @param vmId `i32`
+ * @param value `bool`
+ */
+export declare function setAudioVmPreserveFunctionState(vmId: number, value: boolean): void;
+/**
+ * as/assembly/audio-vm/constants/bpmOverride
+ * @param value `f32`
+ */
+export declare function bpmOverride(value: number): void;
+/**
+ * as/assembly/kernel/sample-record/initSampleRecord
+ * @param setupPtr `usize`
+ * @param setupLength `i32`
+ * @param loopPtr `usize`
+ * @param loopLength `i32`
+ * @param numSamples `i32`
+ * @param sampleRate `f32`
+ */
+export declare function initSampleRecord(setupPtr: number, setupLength: number, loopPtr: number, loopLength: number, numSamples: number, sampleRate: number): void;
+/**
+ * as/assembly/kernel/sample-record/runSampleRecordSetup
+ */
+export declare function runSampleRecordSetup(): void;
+/**
+ * as/assembly/kernel/sample-record/recordSampleChunk
+ * @param chunkSize `i32`
+ * @returns `i32`
+ */
+export declare function recordSampleChunk(chunkSize: number): number;
+/**
+ * as/assembly/kernel/sample-record/recordSampleAll
+ */
+export declare function recordSampleAll(): void;
+/**
+ * as/assembly/kernel/sample-record/getSampleRecordOutputPtr
+ * @returns `usize`
+ */
+export declare function getSampleRecordOutputPtr(): number;
+/**
+ * as/assembly/kernel/sample-record/getSampleRecordSamplesRecorded
+ * @returns `i32`
+ */
+export declare function getSampleRecordSamplesRecorded(): number;
+/**
+ * as/assembly/kernel/sample-record/isSampleRecordComplete
+ * @returns `bool`
+ */
+export declare function isSampleRecordComplete(): boolean;
+/**
+ * as/assembly/kernel/sample-record/resetSampleRecord
+ */
+export declare function resetSampleRecord(): void;
+/**
+ * as/assembly/kernel/sample-record/ensureSampleRecordGlobalsSize
+ * @param size `i32`
+ */
+export declare function ensureSampleRecordGlobalsSize(size: number): void;
+/**
+ * as/assembly/kernel/sample-record/setSampleRecordGlobal
+ * @param index `i32`
+ * @param value `f32`
+ */
+export declare function setSampleRecordGlobal(index: number, value: number): void;
+/**
+ * as/assembly/kernel/sample-record/setSampleRecordGlobalUndefined
+ * @param index `i32`
+ */
+export declare function setSampleRecordGlobalUndefined(index: number): void;
+/**
+ * as/assembly/mini/index/generateMiniHistoryWindow
+ * @param bytecode$ `usize`
+ * @param history$ `usize`
+ * @param windowStartSample `i32`
+ * @param windowEndSample `i32`
+ * @param bpm `f32`
+ * @param sampleRate `f32`
+ * @param barValue `f32`
+ */
+export declare function generateMiniHistoryWindow(bytecode$: number, history$: number, windowStartSample: number, windowEndSample: number, bpm: number, sampleRate: number, barValue: number): void;
---
+++ as/build/index-mobile.js
@@ -0,0 +1,153 @@
+import * as __import0 from "sample";
+import * as __import1 from "debug";
+async function instantiate(module, imports = {}) {
+  const __module0 = imports.sample;
+  const __module1 = imports.debug;
+  const adaptedImports = {
+    env: Object.setPrototypeOf({
+      abort(message, fileName, lineNumber, columnNumber) {
+        // ~lib/builtins/abort(~lib/string/String | null?, ~lib/string/String | null?, u32?, u32?) => void
+        message = __liftString(message >>> 0);
+        fileName = __liftString(fileName >>> 0);
+        lineNumber = lineNumber >>> 0;
+        columnNumber = columnNumber >>> 0;
+        (() => {
+          // @external.js
+          throw Error(`${message} in ${fileName}:${lineNumber}:${columnNumber}`);
+        })();
+      },
+      "console.log"(text) {
+        // ~lib/bindings/dom/console.log(~lib/string/String) => void
+        text = __liftString(text >>> 0);
+        console.log(text);
+      },
+    }, Object.assign(Object.create(globalThis), imports.env || {})),
+    sample: Object.setPrototypeOf({
+      readSampleChunk(sampleHandle, channel, startSample, length, destPtr) {
+        // as/assembly/kernel/sampler/readSampleChunk(i32, i32, i32, i32, usize) => void
+        destPtr = destPtr >>> 0;
+        __module0.readSampleChunk(sampleHandle, channel, startSample, length, destPtr);
+      },
+    }, __module0),
+    debug: __module1,
+  };
+  const { exports } = await WebAssembly.instantiate(module, adaptedImports);
+  const memory = exports.memory || imports.env.memory;
+  const adaptedExports = Object.setPrototypeOf({
+    createFloat32Buffer(size) {
+      // as/assembly/index/createFloat32Buffer(i32) => usize
+      return exports.createFloat32Buffer(size) >>> 0;
+    },
+    memoryUsage() {
+      // as/assembly/index/memoryUsage() => usize
+      return exports.memoryUsage() >>> 0;
+    },
+    getFloat32BufferArenaInfo() {
+      // as/assembly/index/getFloat32BufferArenaInfo() => usize
+      return exports.getFloat32BufferArenaInfo() >>> 0;
+    },
+    getAudioVmInfo() {
+      // as/assembly/index/getAudioVmInfo() => usize
+      return exports.getAudioVmInfo() >>> 0;
+    },
+    getAudioVmInfoAt(vmId) {
+      // as/assembly/index/getAudioVmInfoAt(i32) => usize
+      return exports.getAudioVmInfoAt(vmId) >>> 0;
+    },
+    getAudioVmArenaInfoAt(vmId) {
+      // as/assembly/index/getAudioVmArenaInfoAt(i32) => usize
+      return exports.getAudioVmArenaInfoAt(vmId) >>> 0;
+    },
+    getAudioVmArrayElementIsUndefined(vmId, arrayGlobalIndex, elementIndex) {
+      // as/assembly/index/getAudioVmArrayElementIsUndefined(i32, i32, i32) => bool
+      return exports.getAudioVmArrayElementIsUndefined(vmId, arrayGlobalIndex, elementIndex) != 0;
+    },
+    getAudioVmNestedArrayElementIsUndefined(vmId, outerArrayGlobalIndex, outerIndex, innerIndex) {
+      // as/assembly/index/getAudioVmNestedArrayElementIsUndefined(i32, i32, i32, i32) => bool
+      return exports.getAudioVmNestedArrayElementIsUndefined(vmId, outerArrayGlobalIndex, outerIndex, innerIndex) != 0;
+    },
+    setAudioVmPreserveFunctionState(vmId, value) {
+      // as/assembly/index/setAudioVmPreserveFunctionState(i32, bool) => void
+      value = value ? 1 : 0;
+      exports.setAudioVmPreserveFunctionState(vmId, value);
+    },
+    getSampleRecordOutputPtr() {
+      // as/assembly/kernel/sample-record/getSampleRecordOutputPtr() => usize
+      return exports.getSampleRecordOutputPtr() >>> 0;
+    },
+    isSampleRecordComplete() {
+      // as/assembly/kernel/sample-record/isSampleRecordComplete() => bool
+      return exports.isSampleRecordComplete() != 0;
+    },
+  }, exports);
+  function __liftString(pointer) {
+    if (!pointer) return null;
+    const
+      end = pointer + new Uint32Array(memory.buffer)[pointer - 4 >>> 2] >>> 1,
+      memoryU16 = new Uint16Array(memory.buffer);
+    let
+      start = pointer >>> 1,
+      string = "";
+    while (end - start > 1024) string += String.fromCharCode(...memoryU16.subarray(start, start += 1024));
+    return string + String.fromCharCode(...memoryU16.subarray(start, end));
+  }
+  return adaptedExports;
+}
+export const {
+  memory,
+  __new,
+  __pin,
+  __unpin,
+  __collect,
+  __rtti_base,
+  createFloat32Buffer,
+  freeFloat32Buffer,
+  memoryUsage,
+  memoryGrow,
+  getFloat32BufferArenaInfo,
+  runAudioVm,
+  getAudioVmInfo,
+  runAudioVmAt,
+  getAudioVmInfoAt,
+  getAudioVmArenaInfoAt,
+  releaseAudioVmOutputsAt,
+  copyAudioVmState,
+  resetAudioVmAt,
+  getAudioVmGlobalAt,
+  setAudioVmGlobalAt,
+  setAudioVmGlobalUndefined,
+  setAudioVmGlobalsSize,
+  getAudioVmArrayElementAt,
+  getAudioVmArrayElementIsUndefined,
+  getAudioVmNestedArrayElementAt,
+  getAudioVmNestedArrayElementIsUndefined,
+  setAudioVmOversampleModes,
+  setAudioVmPreserveFunctionState,
+  bpmOverride,
+  initSampleRecord,
+  runSampleRecordSetup,
+  recordSampleChunk,
+  recordSampleAll,
+  getSampleRecordOutputPtr,
+  getSampleRecordSamplesRecorded,
+  isSampleRecordComplete,
+  resetSampleRecord,
+  ensureSampleRecordGlobalsSize,
+  setSampleRecordGlobal,
+  setSampleRecordGlobalUndefined,
+  generateMiniHistoryWindow,
+} = await (async url => instantiate(
+  await (async () => {
+    const isNodeOrBun = typeof process != "undefined" && process.versions != null && (process.versions.node != null || process.versions.bun != null);
+    if (isNodeOrBun) { return globalThis.WebAssembly.compile(await (await import("node:fs/promises")).readFile(url)); }
+    else { return await globalThis.WebAssembly.compileStreaming(globalThis.fetch(url)); }
+  })(), {
+    sample: __maybeDefault(__import0),
+    debug: __maybeDefault(__import1),
+  }
+))(new URL("index-mobile.wasm", import.meta.url));
+function __maybeDefault(module) {
+  return typeof module.default === "object" && Object.keys(module).length == 1
+    ? module.default
+    : module;
+}
